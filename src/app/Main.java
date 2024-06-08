f256e39fa7e5a891562fec18dc821ee94538add6
Yuliia Deineka <yu.deyneka@gmail.com>
2024-06-09 00:37:36 +0200

    Add mile to kilometre convert

--- src/app/Main.java
+++ src/app/Main.java
@@ -2,7 +2,19 @@ package app;
 
 public class Main {
 
+    private static final double KONV_D = 1.60934;
+
     public static void main(String[] args) {
         System.out.println("My Metric Conversions app. ");
+
+        double mile = 10;
+        double kilometre = konvMlsToKm(mile);
+
+        System.out.println("Result is " + kilometre + " kilometre.");
+
+    }
+
+    private static double konvMlsToKm(double mile) {
+        return mile * KONV_D;
     }
 }
